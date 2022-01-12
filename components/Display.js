1b254eb58c55f63c91c0e506e19b1916ee4be77e
Sayandip Halder <sayandiph4@gmail.com>
2022-01-12 23:20:54 +0530

    add space between flatlists

--- components/Display.js
+++ components/Display.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, ActivityIndicator, Button } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, Button, Modal, StyleSheet, Pressable } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
 export default class Display extends Component {
@@ -10,6 +10,7 @@ export default class Display extends Component {
       loading: false,
       data: [],
       error: null,
+      modalVisible: false,
     };
 
     this.arrayholder = [];
@@ -17,6 +18,10 @@ export default class Display extends Component {
 
   componentDidMount() {
     this.apiResponse();
+  }
+
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
   }
 
   apiResponse = () => {
@@ -80,18 +85,90 @@ export default class Display extends Component {
         <FlatList
           data={this.state.data}
           renderItem={({ item }) => ( 
-              <Button
-                onPress={() => {
-                    alert('You tapped the button!');
-                }}
-                color={item.CLOSE >= item.OPEN? "green": "red"}
-                title={item.SYMBOL}
-            />
+              <>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        this.setModalVisible(!this.state.modalVisible);
+                    }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Hello World!</Text>
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => this.setModalVisible(!this.state.modalVisible)}
+                        
+                    >
+                        <Text style={styles.textStyle}>Hide info</Text>
+                    </Pressable>
+                    </View>
+                </View>
+                </Modal>
+
+                <Pressable
+                    style={[styles.button, styles.buttonOpen,{backgroundColor: item.CLOSE >= item.OPEN? "green": "red"}]}
+                    onPress={() => this.setModalVisible(true)}
+                >
+                    <Text style={styles.textStyle}>{item.SYMBOL}</Text>
+                </Pressable>
+            </>
           )}
           keyExtractor={(x,i) => i}
           ListHeaderComponent={this.renderHeader}
+          ItemSeparatorComponent={
+            () => <View style={{height:5}} />
+        }
         />
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
+  
