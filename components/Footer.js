beca076aba482a6b7c3a3e9b6ddb99dca0e05159
Sayandip Halder <sayandiph4@gmail.com>
2022-01-13 00:21:35 +0530

    add fontawesome icons

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ExploreScreen from './screens/ExploreScreen';
 
 //React native icons
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // React navigation
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -18,9 +18,31 @@ export default function Footer () {
     return(
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Profile" component={ProfileScreen} />
-                <Tab.Screen name="Explore" component={ExploreScreen} />
+                <Tab.Screen name="Home" component={HomeScreen} 
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: () => (
+                        <Icon name="home" color="black" size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen name="Explore" component={ExploreScreen}
+                    options={{
+                        tabBarLabel: 'Explore',
+                        tabBarIcon: () => (
+                          <Icon name="search" color="black" size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen name="Profile" component={ProfileScreen}
+                    options={{
+                        tabBarLabel: 'Profile',
+                        tabBarIcon: () => (
+                        <Icon name="user-circle" color="black" size={30} />
+                        ),
+                    }}
+                />
+
             </Tab.Navigator>
         </NavigationContainer>
     )
